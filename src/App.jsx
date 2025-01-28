843e5cf29e48c75ab3ec6eae133cced8a10c3fac
Pavel_Melnik <129269794+miller-pavel@users.noreply.github.com>
2025-01-28 17:04:59 +0200

    svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/lingo.svg";
-import viteLogo from "/vite.svg";
+import viteLogo from "/lingolearn.svg";
 import "./App.css";
 
 function App() {
